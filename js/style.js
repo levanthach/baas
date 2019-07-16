b1a8ae5722ce980c16188927bae6c4912100790b
lethach <lethach150699@gmail.com>
2019-07-16 17:43:04 +0700

    Udate source 16/7

--- js/style.js
+++ js/style.js
@@ -3,6 +3,23 @@
 	  "insertBy": "embed",
 	  "attribute": "data-youtube"
 	});
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    dots:false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	})
 });
 
 (function ($) {
